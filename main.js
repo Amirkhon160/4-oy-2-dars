926a7434a106a7de3444de180e72976c97334ae0
Amirkhon160 <amirkhoninagamov001@gmail.com>
2024-07-21 16:57:42 +0500

    uyga vazifa

---
+++ main.js
@@ -0,0 +1,5 @@
+const username = prompt(`string type dagi malumot kirgizing`)
+const newNumber = prompt(`number type dagi malumot kirgizing`)
+const boolean = prompt(`Boolean typega tegishli so'z yozing`)
+alert(`siz kiritgan ma'lumot ${typeof (username)} typega tegishli \n siz kiritgan ma'lumot  ${typeof (Number(newNumber))} typega tegishli \n
+siz kiritgan so'z ${typeof (Boolean(boolean))} typega tegishli`)
